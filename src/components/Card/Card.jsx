cb5a76f8643f6b9ae1851b1cf6692108b1b220f1
Steven Bryant <stevelad_83@yahoo.com>
2024-04-17 09:36:39 -0400

    styling Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,13 +5,14 @@ import { Link } from "react-router-dom";
 export default function Card({ title, imageUrl, description, id, price }) {
   return (
     <div className="card">
-      <Link
-        to={`/details/${id}`}
-        style={{ textDecoration: "none", color: "inherit" }}
-      >
-        <img src={imageUrl} alt="placeholder kitten" />
-        <h3 className="description">{description}</h3>
-        <p>{price}</p>
+      <Link to={`/details/${id}`} className="card-link">
+        <div className="content">
+          <img src={imageUrl} alt="placeholder kitten" />
+          <div className="text-content">
+            <h3 className="description">{description}</h3>
+            <h2 className="price">{price}</h2>
+          </div>
+        </div>
       </Link>
     </div>
   );
