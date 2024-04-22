a0688410060165459e2f3d4983e0cea11bc13714
Steven Bryant <stevelad_83@yahoo.com>
2024-04-22 09:41:04 -0400

    added image list and main image to Details component

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { cards } from "../../assets/data.jsx";
 import { useParams } from "react-router-dom";
+import "./Details.css";
 
 export default function Details(props) {
   const { id } = useParams();
   const card = cards.find((card) => card.id === parseInt(id));
-  console.log(cards);
-  console.log(id);
+  const mainImageUrl =
+    card.images && card.images.length > 0
+      ? card.images[0].imageUrl
+      : card.imageUrl;
+
   return (
-    <div>
+    <div className="details_info">
+      <div className="details_images">
+        <ul className="image_list">
+          {[...Array(6)].map((_, index) => (
+            <li key={index}>
+              <img src={mainImageUrl} alt={`placeholder kitten ${index}`} />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="main_img">
+        <img src={mainImageUrl} alt="main kitten" />
+      </div>
       <p>{card.details}</p>
     </div>
   );
